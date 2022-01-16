f2b93cf3198c78d5d7b89b673bdafd030c288907
Sharon-Korir <sharon.korir@student.moringaschool.com>
2022-01-16 23:05:36 +0300

    add toppings object

--- js/scripts.js
+++ js/scripts.js
@@ -30,18 +30,53 @@ function submitForm (event){
   function Pizza(size, crust, toppings) {
     this.size = size;
     this.crust = crust;
-    this.toppings = toppings;
+    /*this.toppings = ;*/
   }
   var size = document.getElementById("size").value;
   var crust = document.getElementById("crust").value;
   //get toppings
-  var toppings = [bacon, chicken, steak, mushrooms, capsicum, corn];
-  var bacon = document.getElementById("bacon").value;
-  var chicken = document.getElementById("chicken").value;
-  var steak = document.getElementById("steak").value;
-  var mushrooms = document.getElementById("mushrooms").value;
-  var capsicum = document.getElementById("capsicum").value;
-  var corn = document.getElementById("corn").value;
+
+  function getValue() {
+    var topping = [];
+    var bacon = document.getElementById("bacon")
+    if(bacon.checked){
+      console.log("bacon");
+      topping.push(bacon.value);
+    }
+    var chicken = document.getElementById("chicken")
+    if(chicken.checked){
+      topping.push(chicken.value);
+    }
+    var steak = document.getElementById("steak")
+    if(steak.checked){
+      topping.push(steak.value);
+    }
+    var mushrooms = document.getElementById("mushrooms")
+    if(mushrooms.checked){
+      topping.push(mushrooms.value);
+    }
+    var capsicum = document.getElementById("capsicum")
+    if(capsicum.checked){
+      topping.push(capsicum.value);
+    }
+    var corn = document.getElementById("corn")
+    if(corn.checked){
+      topping.push(corn.value);
+    }
+    console.log(topping);
+  }
+  
+  var getToppings =  getValue();
+  console.log(getToppings)
+
+  /*$(function(getToppings){
+    $("#checkout").click(function(){
+      var val = [];
+      $(":checkbox:checked").each(function(i){
+        val[i] = $(this).val();
+      });
+    });
+  });*/
 
   var pizzaOrder = new Pizza(size, crust, toppings)
   console.log(pizzaOrder);
