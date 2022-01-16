2886e7cf8ad1529c918dc89009473e6ffe9b30f8
Sharon-Korir <sharon.korir@student.moringaschool.com>
2022-01-16 16:20:42 +0300

    add output form and hide

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 //user interface logic
 $(document).ready(function(){
-  $(".order").click(function(){
-    $(".order-hidden").toggle();
+  $("#submit").click(function(){
+    $(".box-hidden").hide();
   });
+  $("#submit").click(function(){
+    $(".box-showing").show();
+  });
+  $("#checkout").click(function(){
+    $(".box-showing").hide();
+  });
+  $("#checkout").click(function(){
+    $(".ordertotal").show()
+  });
+  event.preventDefault();
 });
