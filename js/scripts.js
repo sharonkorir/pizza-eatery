c4c7f788be1a7ca1a0242ceae511a5b6626ade46
Sharon-Korir <sharon.korir@student.moringaschool.com>
2022-01-16 22:01:52 +0300

    add pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -25,5 +25,24 @@ $(document).ready(function(){
 
 //business logic
 function submitForm (event){
-  
+  event.preventDefault();
+  console.log("test");
+  function Pizza(size, crust, toppings) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+  }
+  var size = document.getElementById("size").value;
+  var crust = document.getElementById("crust").value;
+  //get toppings
+  var toppings = [bacon, chicken, steak, mushrooms, capsicum, corn];
+  var bacon = document.getElementById("bacon").value;
+  var chicken = document.getElementById("chicken").value;
+  var steak = document.getElementById("steak").value;
+  var mushrooms = document.getElementById("mushrooms").value;
+  var capsicum = document.getElementById("capsicum").value;
+  var corn = document.getElementById("corn").value;
+
+  var pizzaOrder = new Pizza(size, crust, toppings)
+  console.log(pizzaOrder);
 }
